4e29b549a1ec9950e196ea1772785434f6e294dd
aasimsyed97 <sayedaasim555@gmail.com>
2022-10-01 13:39:17 +0530

    first logo

--- UFC280/slidshow.js
+++ UFC280/slidshow.js
@@ -1,20 +1,21 @@
   let i=0; 
   let images = JSON.parse(localStorage.getItem("images")); 
-
+  let cont = document.getElementById("container");
  function start() { 
    
   
     setInterval(function(){  
-        let cont = document.getElementById("container");
+        if(i==images.length){ 
+            i=0;
+        }
+        cont.innerHTML = null;
     let image = document.createElement("img");
     image.src = images[i] 
     console.log(i)
     cont.append(image)   
        i++; 
-       if(i==images.length){ 
-           i=0;
-       }
-    },1500)
+      
+    },1000)
  } 
   
  
